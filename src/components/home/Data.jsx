89773a63479e8eb7b9baa49e211fd3aac74abb31
Jenyffer <jenyfferbastos06@gmail.com>
2023-01-27 18:32:40 -0300

    feat: add data component of home component

---
+++ src/components/home/Data.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export function Data() {
+  return (
+    <div className="home_data">
+      <h1 className="home_title">Jenyffer Bastos</h1>
+      <h3 className="home_subtitle">Software Developer</h3>
+      <p className="home_description">
+        I'm a developer focused on Front End, and very passionate and dedicated to
+        my work.
+      </p>
+      <a href="#contact" className="button button--flex">
+        Say Hello
+      </a>
+    </div>
+  );
+}
